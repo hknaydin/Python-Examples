869f8e7453fd2ef33e456cd0de63d8f8afe0d151
Hakan AYDIN <hakayd28@gmail.com>
2020-04-16 11:40:38 +0300

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,27 +1,28 @@
-def isPrime(num):
-  divisorNumber = 0
-  # burada num sayısının bölen sayısını bulunuz.
-  # ...,
-  for i in range(2,int(num)):
-    if num % i == 0:
-      divisorNumber = divisorNumber + 1
-
-  if divisorNumber == 0:
-    return True
-  else:
+def isPrime(num): # num: parametre
+  if num<2:   # 2 den küçükse asal değil
     return False
-
-n1 = 19
-print(n1,isPrime(n1))  # ekran: 19, True olmalı
-
+  elif num != int(num):  # ondalık sayı ise asal değil
+    return False
+  else:  # normal bir sayının testi
+    divNum = 0
+    for i in range(2,num-1):
+      if (num%i == 0):
+        divNum = divNum + 1
+    if divNum == 0:
+      return True
+    else:
+      return False
 ##############################################################
-
 def allPrimes(n):
   lst = []
-  for i in range(2, n) :
-
-    if isPrime(i) :
+  for i in range(2,n):
+    if isPrime(i):
       lst.append(i)
-  print(lst)
-  
-allPrimes(100)
+  return lst
+##############################################################
+n1 = 20
+print(isPrime(n1)) # n1 değişkeni argüman dır
+
+n2 = 100
+lst2 = allPrimes(n2)
+print(lst2)
